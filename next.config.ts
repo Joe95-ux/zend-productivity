b525dfee4f6209fb2899143be567f5506e7f58e9
joe95-ux <ogorktabi@gmail.com>
2025-11-03 03:37:41 +0200

    fix prisma bug

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    // Ignore WASM files that Prisma might try to import
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ".prisma/client/wasm-edge-light-loader": false,
+      "./query_engine_bg.wasm?module": false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
